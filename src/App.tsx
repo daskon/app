049495f0fec925b9fa74df62d0402d521056d981
daskon <aselaforever@gmail.com>
2022-11-24 14:19:19 +0530

    excel upload form added

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,18 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>Upload Excel File</h1>
+        <div className="container">
+        <form className="form">
+          <div className="form-group">
+            <label htmlFor="excel-file">Choose File</label>
+            <input
+              type="file"
+              id="excel-file"
+              className="form-control"
+              />
+          </div>
+        </form>
+      </div>
       </header>
     </div>
   );
